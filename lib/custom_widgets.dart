50885577bc026f4b86b416803f079f9a6316e9b4
Dylan Drost <dyla.drost@gmail.com>
2018-06-22 15:18:40 +0200

    Finished.
    We can now show the details of a selected movie in a new screen and share that with other people.
    
    We could add more info now like show the cast of the movie or show more image like movie posters or backdrops.

--- lib/custom_widgets.dart
+++ lib/custom_widgets.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:star_wars_movies/models/models.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class LoadingSpinner extends StatelessWidget {
   final String text;
@@ -52,5 +54,245 @@ class ActionItemWidget extends StatelessWidget {
       tooltip: tooltip,
       onPressed: onPressed,
     );
+  }
+}
+
+class MovieTitleColumn extends StatelessWidget {
+  final String title;
+
+  const MovieTitleColumn({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: textTheme.title,
+            textAlign: TextAlign.start,
+            softWrap: true,
+          )
+        ]);
+  }
+}
+
+class RatingsRow extends StatelessWidget {
+  final double voteAverage;
+  final int voteCount;
+
+  const RatingsRow(
+      {Key key, @required this.voteAverage, @required this.voteCount})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        RatingsColumn(
+          value: '$voteAverage',
+          label: 'Rating',
+        ),
+        RatingsColumn(
+          value: '$voteCount',
+          label: 'Votes',
+        ),
+      ],
+    );
+  }
+}
+
+class RatingsColumn extends StatelessWidget {
+  final String value;
+  final String label;
+
+  const RatingsColumn({Key key, @required this.value, @required this.label})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Padding(
+        padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              value,
+              style: textTheme.caption,
+            ),
+            Text(
+              label,
+              style: textTheme.caption,
+            )
+          ],
+        ));
+  }
+}
+
+class GenreWidget extends StatelessWidget {
+  final List<Genre> genres;
+
+  const GenreWidget({Key key, @required this.genres}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+      child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.start,
+          runSpacing: 6.0,
+          spacing: 6.0,
+          children: List.generate(
+              genres.length,
+              (index) => Chip(
+                    label: Text(genres[index].name),
+                  ))),
+    );
+  }
+
+  List<Widget> genreWidgets(List<Genre> genres) => List.generate(
+      genres.length,
+      (index) => Chip(
+            label: Text(genres[index].name),
+          ));
+}
+
+class ReviewsWidget extends StatelessWidget {
+  final List<Review> reviews;
+
+  const ReviewsWidget({Key key, @required this.reviews}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Reviews',
+                  style: textTheme.headline,
+                )
+              ],
+            ),
+          ),
+        ]..addAll(buildReviewsList(reviews)),
+      ),
+    );
+  }
+
+  List<Widget> buildReviewsList(List<Review> reviews) => List.generate(
+      reviews.length, (index) => buildReviewRowWidget(reviews[index]));
+
+  Widget buildReviewRowWidget(Review review) {
+    return Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(review.author),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                ),
+                child: Text(
+                  review.content,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: true,
+                  maxLines: 6,
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class FlexSpaceAppBar extends StatelessWidget {
+  final String title;
+  final String path;
+  final double appBarHeight;
+
+  const FlexSpaceAppBar(
+      {Key key,
+      @required this.title,
+      @required this.path,
+      @required this.appBarHeight})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlexibleSpaceBar(
+      title: Text(
+        title,
+      ),
+      background: HeroImageWidget(path: path, appBarHeight: appBarHeight),
+    );
+  }
+}
+
+class HeroImageWidget extends StatelessWidget {
+  final String path;
+  final double appBarHeight;
+
+  const HeroImageWidget(
+      {Key key, @required this.path, @required this.appBarHeight})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        FadeInImage.memoryNetwork(
+          placeholder: kTransparentImage,
+          image: "https://image.tmdb.org/t/p/w780/$path",
+          height: appBarHeight,
+          fit: BoxFit.fitHeight,
+        ),
+        // This gradient ensures that the toolbar icons are distinct
+        // against the background image.
+        const DecoratedBox(
+          decoration: const BoxDecoration(
+            gradient: const LinearGradient(
+              begin: const FractionalOffset(0.5, 0.0),
+              end: const FractionalOffset(0.5, 0.30),
+              colors: const <Color>[
+                const Color(0x60000000),
+                const Color(0x00000000)
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextOverviewWidget extends StatelessWidget {
+  const TextOverviewWidget({
+    Key key,
+    @required this.text,
+    @required this.style,
+  }) : super(key: key);
+
+  final String text;
+  final TextStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          text,
+          style: style,
+        ));
   }
 }
--- lib/movie_page.dart
+++ lib/movie_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:share/share.dart';
+import 'package:star_wars_movies/custom_widgets.dart';
 import 'package:star_wars_movies/models/models.dart';
 import 'package:star_wars_movies/services/api.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MoviePage extends StatelessWidget {
   final ApiClient _client = ApiClient();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
   final MovieSearchResult movieSearchResult;
 
@@ -21,26 +25,69 @@ class MoviePage extends StatelessWidget {
     return FutureBuilder<Movie>(
       future: _client.getMovie(searchResult.id),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        // return which depending on state and data.
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return MinimalMovieInfo(
+                movie: movieSearchResult, scaffoldKey: scaffoldKey);
+          default:
+            if (snapshot.hasError)
+              return Text('Error: ${snapshot.error}');
+            else
+              return FullMovieInfo(
+                  movie: snapshot.data, scaffoldKey: scaffoldKey);
+        }
       },
     );
   }
 }
 
 class MinimalMovieInfo extends StatelessWidget {
+  final double appBarHeight = 256.0;
   final MovieSearchResult movie;
+  final GlobalKey<ScaffoldState> scaffoldKey;
 
   const MinimalMovieInfo({
     Key key,
     @required this.movie,
+    @required this.scaffoldKey,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // build widget showing movie data.
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: appBarHeight,
+          pinned: true,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.share), onPressed: _onSharePressed)
+          ],
+          flexibleSpace: FlexSpaceAppBar(
+              title: "",
+              path: movie.backdropPath ?? movie.posterPath,
+              appBarHeight: appBarHeight),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate(<Widget>[
+          MovieSpecsView(
+            title: movie.title,
+            posterPath: movie.posterPath,
+            voteAverage: movie.voteAverage,
+            voteCount: movie.voteCount,
+            key: Key(movie.title),
+          ),
+          TextOverviewWidget(text: movie.overview, style: textTheme.body1),
+          LoadingSpinner(text: 'loading'),
+        ])),
+      ],
+    );
   }
 
-  void _onSharePressed() {}
+  void _onSharePressed() {
+    Share.share("Check out ${movie.title}");
+  }
 }
 
 class FullMovieInfo extends StatelessWidget {
@@ -56,10 +103,45 @@ class FullMovieInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // build widget showing movie data.
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return CustomScrollView(
+      scrollDirection: Axis.vertical,
+      primary: true,
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: appBarHeight,
+          pinned: true,
+          actions: <Widget>[
+            ActionItemWidget(
+                icon: Icons.share, onPressed: _onSharePressed, tooltip: "Share")
+          ],
+          flexibleSpace: FlexSpaceAppBar(
+              title: "",
+              path: movie.backdropPath ?? movie.posterPath,
+              appBarHeight: appBarHeight),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate(<Widget>[
+          MovieSpecsView(
+            key: Key(movie.title),
+            title: movie.title,
+            posterPath: movie.posterPath,
+            voteAverage: movie.voteAverage,
+            voteCount: movie.voteCount,
+            genres: movie.genres,
+          ),
+          TextOverviewWidget(text: movie.tagline, style: textTheme.body2),
+          TextOverviewWidget(text: movie.overview, style: textTheme.body1),
+          ReviewsWidget(reviews: movie.reviews.results),
+        ])),
+      ],
+    );
   }
 
-  void _onSharePressed() {}
+  void _onSharePressed() {
+    Share.share(
+        "You should check ${movie.title} you can find more info at ${movie.homepage}");
+  }
 }
 
 class MovieSpecsView extends StatelessWidget {
@@ -80,7 +162,43 @@ class MovieSpecsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Build widget to show the title, poster, ratings and genres.
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: FadeInImage.memoryNetwork(
+            key: Key(posterPath),
+            placeholder: kTransparentImage,
+            image: 'https://image.tmdb.org/t/p/w500/$posterPath',
+            width: 130.0,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: _buildChildren(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> _buildChildren() {
+    var children = <Widget>[
+      MovieTitleColumn(title: title),
+      RatingsRow(voteAverage: voteAverage, voteCount: voteCount)
+    ];
+
+    if (genres != null && genres.isNotEmpty) {
+      children.add(GenreWidget(genres: genres));
+    }
+    return children;
   }
 }
 
